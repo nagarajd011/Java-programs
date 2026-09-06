4bf05ad3e156110d1f53f1f9e59dde491b502f8f
nagarajd11 <nagarajd1126@gmail.com>
2026-09-06 17:39:06 +0530

    Spy numbers

---
+++ Program6.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class SpyNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        int original = num;
+
+        int sum = 0;
+        int product = 1;
+
+        while (num != 0) {
+            int digit = num % 10;
+            sum = sum + digit;
+            product = product * digit;
+            num = num / 10;
+        }
+
+        if (sum == product) {
+            System.out.println(original + " is a Spy Number");
+        } else {
+            System.out.println(original + " is not a Spy Number");
+        }
+
+        sc.close();
+    }
+}
